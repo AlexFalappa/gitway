1441029ad34a4ce09be1ac6a554e52c71d9a6106
Alessandro Falappa <alex.falappa@gmail.com>
2014-12-08 17:40:40 +0100

    dummy short route algo

--- way/src/main/java/af/algorithms/ShortRouteCalculator.java
+++ way/src/main/java/af/algorithms/ShortRouteCalculator.java
@@ -1,18 +1,23 @@
 package af.algorithms;
 
 import af.model.Pathway;
+import af.model.WayPoint;
 import java.util.Properties;
 
 public class ShortRouteCalculator implements RouteCalculator {
 
+    Properties props;
+
     @Override
     public Pathway calcRoute(Pathway pw) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        pw.addWaypoint(new WayPoint("one", 0, 0));
+        pw.addWaypoint(new WayPoint("two", 0, 0));
+        return pw;
     }
 
     @Override
     public void setCalcOptions(Properties props) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.props = props;
     }
 
 }
