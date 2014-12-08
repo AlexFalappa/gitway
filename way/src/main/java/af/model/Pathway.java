582a94189e3e2938a2cbb0d0ea1854aeccaeee5f
Alessandro Falappa <alex.falappa@gmail.com>
2014-12-08 17:14:15 +0100

    Added other model object

---
+++ way/src/main/java/af/model/Pathway.java
@@ -0,0 +1,26 @@
+package af.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Pathway model POJO.
+ *
+ * @author Alessandro Falappa <alex.falappa@gmail.com>
+ */
+public class Pathway {
+
+    List<WayPoint> waypoints = new ArrayList<>();
+
+    public void addWaypoint(WayPoint p) {
+        waypoints.add(p);
+    }
+
+    public void delWaypoint(WayPoint p) {
+        waypoints.remove(p);
+    }
+
+    public void clear() {
+        waypoints.clear();
+    }
+}
